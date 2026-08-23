50254b74afd8bcb0bc9acdbedc0885aa3a6784f3
imtugin <imtugin@gmail.com>
2026-08-23 22:36:12 +0300

    Получилось посчитать разницу - не знаю в чём была проблема, но всё работает

--- test/main.go
+++ test/main.go
@@ -4,14 +4,23 @@ import (
 	"fmt"
 )
 
-func main() {
-	var a int
-	fmt.Println("Введите символ. Если введёте не 2, то должна появиться ошибка")
-	fmt.Scanln(&a)
-	switch a {
-	case 2:
-		fmt.Println("Вы ввели", a)
-	default:
-		fmt.Println("ошибка")
+// создаём функцию факториал(перемножение всех чисел от 1 до n, n >1 должно быть, иначе ошибка)
+
+func factorial(n int) (int, interface{}) {
+	var resoult int = 1
+	if n < 1 {
+		return 0, "число должно быть не меншье 1"
+	} else {
+		for i := 1; i <= n; i++ {
+			resoult *= i
+		}
 	}
+	return resoult, nil
+}
+func main() {
+	var f int
+	fmt.Println("Введите число для факториала:")
+	fmt.Scanln(&f)
+	fmt.Println(factorial(f))
+
 }
