3420d1303d7e44bea19dd0f735e1324db0a3d888
imtugin <imtugin@gmail.com>
2026-08-23 10:26:56 +0300

    пока сам не понимаю ничего

--- test/main.go
+++ test/main.go
@@ -1,30 +1,12 @@
 package main
 
-import(
-  "fmt"
-_"errors"
-  )
-//ощибки
-type params_error struct{}
+import (
+	"fmt"
+)
 
-func (error_object params_error)Error()string{
-  return "invalid parametr"
-}
-func divide (a,b int)(int, error){
-  if b <=0{
-    return 0, params_error{}
-  }else{
-    return a/b, nil
-  }
-}
-
-func main (){
-//  obj := params_error{}
-//  fmt.Println(obj.Error())
-  //fmt.Println(params_error{})
-  x, y := 10,5
-  fmt.Println(divide(x,y))
-  y =0
-    fmt.Println(divide(x,y))
-
+func main() {
+	var a int
+	fmt.Println("Введите символ. Если введёте не 2, то должна появиться ошибка")
+	fmt.Scanln(&a)
+	fmt.Println("Вы ввели", a)
 }
