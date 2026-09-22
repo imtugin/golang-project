5b48b965c4de9792c7bc239d45ff07ac885ff112
imtugin <imtugin@gmail.com>
2026-09-22 18:26:08 +0300

    ./test/main.go исправил. Была ошибка в мьютексе

--- test/main.go
+++ test/main.go
@@ -7,19 +7,17 @@ import (
 )
 
 func main() {
-	var money int
-	var donationsCount int
+	var money int32
+	var donationsCount int32
 
 	wg := &sync.WaitGroup{}
 	mutex := &sync.Mutex{}
 
-	wg.Add(1)
 	go func() {
 		for {
 			mutex.Lock()
 			m := money
 			dc := donationsCount
-			fmt.Println(m, dc)
 			if m != dc {
 				fmt.Println("money=", m, "donations=", dc)
 				break
@@ -29,30 +27,15 @@ func main() {
 	}()
 	wg.Add(1000)
 	for range 1000 {
-		mutex.Lock()
 		go func() {
 			defer wg.Done()
+			mutex.Lock()
 			money++
 			donationsCount++
+			mutex.Unlock()
 		}()
-		mutex.Unlock()
 	}
 	wg.Wait()
 	fmt.Println(money)
 
 }
-
-//func main() {
-//	var money int
-//	wg := &sync.WaitGroup{}
-//	wg.Add(1000)
-//	for range 1000 {
-//		go func() {
-//			defer wg.Done()
-//			money++
-//		}()
-//	}
-//	wg.Wait()
-//	fmt.Println(money)
-//
-//}
