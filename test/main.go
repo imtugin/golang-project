abe5a6375a8474a1b3cfe0eeba0ff7219242c72b
imtugin <imtugin@gmail.com>
2026-09-22 19:43:11 +0300

    ./test/main.go решение задачи с измерением времени выполнения и синхронизации функции

--- test/main.go
+++ test/main.go
@@ -2,36 +2,34 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"sync"
+	"time"
 )
 
+var maxWaitSeconds = 5 // задаём максимальное время ожидания выполнения функции
+
+func randomWait() int { // функция совершает работу какую-то
+	//
+	workSeconds := rand.Intn(5 + 1)                      //рандомная генерация времени выполнения которое быдет выполняться функция
+	time.Sleep(time.Duration(workSeconds) * time.Second) //задерживаем функцию на это количество секунд
+	return workSeconds
+
+}
+
 func main() {
-	var money, donationsCount int
+	start := time.Now()
 	wg := &sync.WaitGroup{}
-	mutex := &sync.Mutex{}
-
-	go func() {
-		for {
-			defer wg.Done()
-			mutex.Lock()
-			m := money
-			dc := donationsCount
-			if m != dc {
-				fmt.Println("money=", m, "donations=", donationsCount)
-			}
-			mutex.Unlock()
-		}
-	}()
-	wg.Add(1000)
-	for range 1000 {
-
+	var totalWorkSeconds int // время выполнения main (надо до 5 сек), сколько timesleep в сумме в каждом вызове
+	wg.Add(100)
+	for range 100 {
 		go func() {
 			defer wg.Done()
-			mutex.Lock()
-			money++
-			donationsCount++
-			mutex.Unlock()
+			totalWorkSeconds += randomWait()
 		}()
 	}
-	fmt.Println(money)
+	wg.Wait()
+	mainSeconds := time.Since(start)
+	fmt.Println("main:", mainSeconds)
+	fmt.Println("total:", totalWorkSeconds)
 }
